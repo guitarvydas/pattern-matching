9657751da51582d2082f0776832c14e52a76a9ee
Paul Tarvydas <guitarvydas@gmail.com>
2020-11-19 09:39:14 -0500

    directory moved upwards, change reflected in run.bash

--- foreign/run.bash
+++ foreign/run.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-PROLOG_DIR=../../prolog
+PROLOG_DIR=../prolog
 cat $PROLOG_DIR/support-pre.js $PROLOG_DIR/prolog.js $PROLOG_DIR/support-post.js > ./full-prolog.js
 cat ./full-prolog.js ./support-post2.js ./match.js >temp.js
 node --stack-size=102400 temp.js
